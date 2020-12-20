fd59fc85fd95b6a036d2eeef8d93619c84441acd
김보미 <bomi@Kimbomi-MacBookPro.local>
2020-12-21 00:44:56 +0900

    feat: gradle 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,11 @@
+import com.google.protobuf.gradle.*
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
-    kotlin("jvm") version "1.3.61"
+    kotlin("jvm") version "1.4.10"
+
+    id("com.google.protobuf") version "0.8.14"
+    id("java")
 }
 
 group = "me.study"
@@ -10,14 +16,51 @@ repositories {
 }
 
 dependencies {
-    implementation(kotlin("stdlib-jdk8"))
+    implementation("io.grpc:grpc-all:1.34.1")
+    implementation("io.grpc:grpc-netty-shaded::1.34.1")
+    implementation("javax.annotation:javax.annotation-api:1.3.2")
+
+    testImplementation(kotlin("test-junit5"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
+
 }
 
-tasks {
-    compileKotlin {
-        kotlinOptions.jvmTarget = "1.8"
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "11"
+}
+
+sourceSets {
+    main {
+        java {
+            srcDirs("gen-src/main/java", "gen-src/main/grpc")
+        }
     }
-    compileTestKotlin {
-        kotlinOptions.jvmTarget = "1.8"
+}
+
+
+protobuf {
+    generatedFilesBaseDir = "$projectDir/gen-src"
+
+    protoc {
+        artifact = "com.google.protobuf:protoc:3.10.1"
+    }
+
+    plugins {
+        id("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:1.25.0"
+        }
+    }
+
+    generateProtoTasks {
+        ofSourceSet("main").forEach{
+            it.plugins {
+                id("grpc")
+            }
+        }
     }
 }
