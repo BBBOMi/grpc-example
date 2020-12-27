e122f71d2e239135e187cfc5c252f5d3e069cef7
Bomi <k.bomi096@gmail.com>
2020-12-27 18:13:06 +0900

    chore: gradle version update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
-import com.google.protobuf.gradle.*
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import com.google.protobuf.gradle.*
 
 plugins {
     kotlin("jvm") version "1.4.10"
@@ -17,13 +17,12 @@ repositories {
 
 dependencies {
     implementation("io.grpc:grpc-all:1.34.1")
-    implementation("io.grpc:grpc-netty-shaded::1.34.1")
+    implementation("io.grpc:grpc-netty-shaded:1.34.1")
     implementation("javax.annotation:javax.annotation-api:1.3.2")
 
     testImplementation(kotlin("test-junit5"))
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
-
 }
 
 tasks.test {
